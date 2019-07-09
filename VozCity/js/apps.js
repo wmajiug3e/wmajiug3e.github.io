ac4b6df2f954b18b4424e16a7c373202fc34635d
Oleg <wmajiug3e@gmail.com>
2019-07-09 08:09:11 +0300

    1

--- VozCity/js/apps.js
+++ VozCity/js/apps.js
@@ -7,23 +7,32 @@ $(function () {
 });
 
 $(function () {
-$(document).ready(function() {
-  // executes when HTML-Document is loaded and DOM is ready
-  console.log("document is ready");
+  $.fn.select2.defaults.set( "theme", "bootstrap" );
 
-  $(".navbar-nav").clone().prependTo("#off-canvas");
-
-  $(function() {
-    $(document).trigger("enhance");
+  var placeholder = "не выбрано";
+  
+  $( ".select2-single, .select2-multiple" ).select2( {
+  placeholder: placeholder,
+  width: null,
+  containerCssClass: ':all:'
+  } );
+  
+  $( ".select2-allow-clear" ).select2( {
+  allowClear: true,
+  placeholder: placeholder,
+  width: null,
+  containerCssClass: ':all:'
+  } );
+  
+  $( ".select2-single, .select2-multiple, .select2-allow-clear, .js-data-example-ajax" ).on( "select2:open", function() {
+  if ( $( this ).parents( "[class*='has-']" ).length ) {
+  var classNames = $( this ).parents( "[class*='has-']" )[ 0 ].className.split( /\s+/ );
+  
+  for ( var i = 0; i < classNames.length; ++i ) {
+  if ( classNames[ i ].match( "has-" ) ) {
+  $( "body > .select2-container" ).addClass( classNames[ i ] );
+  }
+  }
+  }
   });
-
-  // document ready
-});
-
-$(window).load(function() {
-  // executes when complete page is fully loaded, including all frames, objects and images
-  console.log("window is loaded");
-
-  // window load
-});
 });
